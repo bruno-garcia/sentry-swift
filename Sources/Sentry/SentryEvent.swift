96a480c9b9418a98516c5dd9ca799aa27b16f165
Lukas Würzburger <lukas.w@me.com>
2021-02-27 13:25:38 +0100

    Add public initializer to SentryEvent

--- Sources/Sentry/SentryEvent.swift
+++ Sources/Sentry/SentryEvent.swift
@@ -5,4 +5,9 @@ public struct SentryEvent {
     public let id: UUID = UUID()
     public var message: String?
     public var tags: Dictionary<String, String>?
+
+    public init(message: String? = nil, tags: Dictionary<String, String>? = nil) {
+        self.message = message
+        self.tags = tags
+    }
 }
